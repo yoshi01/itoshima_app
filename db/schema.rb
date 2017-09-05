ea4823d580ce2495afa99665aa99e2f8ebed5a18
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2017-09-05 17:14:00 +0900

    Add picture uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824082251) do
+ActiveRecord::Schema.define(version: 20170905055341) do
 
   create_table "tourist_spots", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170824082251) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
