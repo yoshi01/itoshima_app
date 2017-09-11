ff121c901aab824d36b94dd9160a158669ba6e63
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2017-09-11 18:03:29 +0900

    Fix admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: [:session]
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
   namespace :admin do
     resources :tourist_spots
     root to: "tourist_spots#index"
