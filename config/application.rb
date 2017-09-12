a921d77e1f36d3b28ea809aaa1526bf0a6acce07
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2017-09-12 17:35:59 +0900

    Add GoogleMap

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
+MAP_API_KEY = ENV['MAP_API_KEY']
+
 module ItoshimaApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
