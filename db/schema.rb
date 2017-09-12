c96b3d91ca1541d0196377a9a6cb3880a5dcd966
yoshi <yoshi@example.com>
2017-09-12 17:35:59 +0900

    Add GoogleMap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907065659) do
+ActiveRecord::Schema.define(version: 20170912060138) do
 
   create_table "tourist_spots", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170907065659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
