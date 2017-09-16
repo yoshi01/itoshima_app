ac1a48e909405c7dd2eda927baa4b3e676b4a9b6
yoshi <yoshi@example.com>
2017-09-16 22:27:36 +0900

    Add tourist_spot marker

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,10 @@
 class MapController < ApplicationController
   def index
+    @tourist_spots = TouristSpot.all
+    @hash = Gmaps4rails.build_markers(@tourist_spots) do |tourist_spot, marker|
+      marker.lat tourist_spot.latitude
+      marker.lng tourist_spot.longitude
+      marker.infowindow tourist_spot.name
+    end
   end
 end
