29446a6d84db06e139601e1d72626183b2b1de6a
yoshi <yoshi@example.com>
2017-09-20 17:35:15 +0900

    Add Route Serch

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,11 +1,18 @@
 class MapController < ApplicationController
   def index
+    start_id = params[:start_id]
+    end_id = params[:end_id]
     @tourist_spots = TouristSpot.all
+    @start_spot = TouristSpot.find(start_id) if start_id.present?
+    @end_spot = TouristSpot.find(end_id) if end_id.present?
     @hash = Gmaps4rails.build_markers(@tourist_spots) do |tourist_spot, marker|
       marker.lat tourist_spot.latitude
       marker.lng tourist_spot.longitude
       marker.infowindow render_to_string( partial: "map/infowindow",
-                                          locals: { tourist_spot: tourist_spot })
+                                          locals: { tourist_spot: tourist_spot,
+                                                    start_id: start_id,
+                                                    end_id: end_id }
+                                        )
     end
   end
 end
