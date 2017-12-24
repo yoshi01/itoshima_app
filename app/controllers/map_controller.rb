7aaf044f012ea78f0e479b207a55c114c0295daa
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2017-12-24 15:31:51 +0900

    Add selectbox of course

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,7 @@
 class MapController < ApplicationController
   def index
     @tourist_spots = TouristSpot.all
+    @courses = Course.all
     @hash = Gmaps4rails.build_markers(@tourist_spots) do |tourist_spot, marker|
       marker.lat tourist_spot.latitude
       marker.lng tourist_spot.longitude
