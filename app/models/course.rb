800ce4588a016dd4e00286e69f5b070955f9fa03
yoshi <yoshi@example.com>
2017-12-24 13:09:50 +0900

    Add association

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+  has_many :course_relationships
+  has_many :tourist_spots, through: :course_relationships
 end
--- app/models/tourist_spot.rb
+++ app/models/tourist_spot.rb
@@ -1,4 +1,6 @@
 class TouristSpot < ApplicationRecord
+  has_many :course_relationships
+  has_many :courses, through: :course_relationships
   mount_uploader :picture, PictureUploader
   paginates_per 10
   geocoded_by :location
