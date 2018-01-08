69b602a4e819a54448556cf0ec8d15d4cb43e79c
yoshi01 <27726838+yoshi01@users.noreply.github.com>
2018-01-08 14:01:53 +0900

    Add marker color change

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -56,11 +56,15 @@ function mapListener(event){
         });
         marker.setMap(map);
         spotMarkers.push(marker);
-        addRoute("選択地点", "(" + roundLat.toString() + ", " + roundLng.toString() + ")", null, lat, lng, markerId);
+        addRoute(null, "選択地点", "(" + roundLat.toString() + ", " + roundLng.toString() + ")", null, lat, lng, markerId);
         markerId = markerId + 1;
         map.setOptions({ draggableCursor: "default"});
         markerMode = false;
     }
+}
+
+function markerChangeListener(spot_id, color){
+    Gmaps.changeMarker(spot_id, color);
 }
 
 function addMarker(){
@@ -70,7 +74,7 @@ function addMarker(){
     }
 }
 
-function addRoute(name, desc, path, lat, lng, marker_id){
+function addRoute(id, name, desc, path, lat, lng, marker_id){
     var tr = $('<tr>');
     var td = $('<td>');
     var tag;
@@ -94,6 +98,10 @@ function addRoute(name, desc, path, lat, lng, marker_id){
       "class": "btn btn-secondary delete-btn",
       text: "×"
     });
+    if (id != null) {
+        google.maps.event.trigger(handler.getMap(), "change", id, "green");
+        $(tr).attr("data-id", id.toString());
+    }
     if (marker_id != null && marker_id != undefined ) {
         $(btn).addClass("marked");
         $(btn).attr("data-marker-id", marker_id.toString());
@@ -147,7 +155,7 @@ function courseChanged(obj) {
             for(var i = 0; i < data.length; i++) {
                 var spot = data[i].tourist_spot;
                 var spot_path = "/tourist_spots/" + spot.id.toString();
-                addRoute(spot.name, spot.description, spot_path,
+                addRoute(spot.id, spot.name, spot.description, spot_path,
                     spot.latitude, spot.longitude)
             }
             searchRoute();
@@ -158,22 +166,26 @@ function courseChanged(obj) {
 }
 
 function clearRoute() {
-    var id;
-    var marked_spots = $('#route-list tr .marked');
-    for (var i = 0; i < marked_spots.length; i++) {
-        id = $(marked_spots[i]).attr("data-marker-id");
-        spotMarkers[id].setMap(null);
+    var route_spots = $('#route-list tr');
+    for (var i = 0; i < route_spots.length; i++) {
+        removeRoute(route_spots[i]);
     }
-    $('#route-list').empty();
 }
 
-$('#route-list').on("click", ".marked", function() {
-    var id = $(this).attr("data-marker-id");
-    spotMarkers[id].setMap(null);
-});
+function removeRoute(tr) {
+    var id = $(tr).attr("data-id");
+    var marker_id = $(tr).find('.marked').attr('data-marker-id');
+    if (id != null && id != undefined) {
+        google.maps.event.trigger(handler.getMap(), "change", id, "red");
+    }
+    if (marker_id != null && marker_id != undefined) {
+        spotMarkers[marker_id].setMap(null);
+    }
+    directionsDisplay.setDirections({routes: []});
+    $(tr).remove();
+    checkDisabled();
+}
 
 $('#route-list').on("click", ".delete-btn", function() {
-    directionsDisplay.setDirections({routes: []});
-    $(this).parents('tr').remove();
-    checkDisabled();
+    removeRoute($(this).parents('tr'));
 });
