5194c6a3bc611f99e4163c2ae13b7615bf4be7ca
yoshi <yoshi@example.com>
2018-01-08 21:43:51 +0900

    Add rank colmun to TouristSpot

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ require "csv"
 CSV.foreach('db/spots_data.csv') do |row|
   TouristSpot.create(:name => row[0], :location => row[1], :description => row[2])
 end
+
+for i in 1..38 do
+  TouristSpot.find(i).update_column(:rank, i)
+end
